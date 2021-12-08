43101c107f6cc89609bf69d204e99c4ef9f300be
mabiane <mabiane.polniak@gmail.com>
2021-12-08 17:04:20 -0300

    add validaçao ao botao salvar

--- src/App.js
+++ src/App.js
@@ -13,18 +13,42 @@ class App extends React.Component {
       cardAttr1: '',
       cardAttr2: '',
       cardAttr3: '',
-      cardRare: 'normal',
+      cardRare: '',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.validateButton = this.validateButton.bind(this);
   }
 
   onInputChange({ target }) {
-    const { value, name } = target;
-    this.setState({ [name]: value });
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, () => {
+      this.setState({ isSaveButtonDisabled: this.validateButton() });
+    });
+  }
+
+  validateButton() { // Nós testamos as condições em que o botão deve estar desabilitado, se o teste for com as condições em que ele é habilitado, logo que um critétio se cumpre ele habilita
+    const {
+      cardName, cardDescription, cardImage, cardAttr1, cardAttr2, cardAttr3, cardRare,
+    } = this.state;
+
+    if (cardName === ''
+    || cardDescription === ''
+    || cardImage === ''
+    || cardRare === '') return true;
+
+    if (cardAttr1 < 0 || cardAttr1 > '90' || cardAttr1 === '') return true;
+    if (cardAttr2 < 0 || cardAttr2 > '90' || cardAttr2 === '') return true;
+    if (cardAttr3 < 0 || cardAttr3 > '90' || cardAttr3 === '') return true;
+    if ((Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3)) > '210') return true;
+
+    return false;
   }
 
   render() {
+    const { isSaveButtonDisabled } = this.state;
     return (
       <body>
         <header>
@@ -35,7 +59,11 @@ class App extends React.Component {
           </div>
         </header>
         <main>
-          <Form onInputChange={ this.onInputChange } />
+          <Form
+            onInputChange={ this.onInputChange }
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+          />
+
           <Card { ...this.state } />
         </main>
       </body>

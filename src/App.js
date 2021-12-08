48ce57d81a6c8f11296d1da93e973e251a9a7b37
mabiane <mabiane.polniak@gmail.com>
2021-12-08 15:31:58 -0300

    add formulario e estilizaçao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Form from './components/Form';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Tryunfo</h1>
+      <body>
+        <header>
+          <h1>Tryunfo</h1>
+          <div>
+            <h2> Criador de NPC`s </h2>
+          </div>
+        </header>
+        <main>
+          <Form />
+        </main>
+      </body>
     );
   }
 }
---
+++ src/components/Form.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../css/form.css';
+
+class Form extends React.Component {
+  render() {
+    const {
+      cardName, cardDescription, cardImage, cardAttr1,
+      cardAttr2, cardAttr3, cardRare, cardTrunfo, onInputChange,
+      isSaveButtonDisabled, hasTrunfo, onSaveButtonClick,
+    } = this.props;
+
+    return (
+      <section>
+        <form className="tabela">
+          <fieldset>
+            <legend>Nome</legend>
+            <input
+              type="text"
+              data-testid="name-input"
+              value={ cardName }
+              name="cardName"
+              onChange={ onInputChange }
+              placeholder="Yaren"
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>História</legend>
+            <textarea
+              data-testid="description-input"
+              placeholder="Halfling - pés leves"
+              value={ cardDescription }
+              name="cardDescription"
+              onChange={ onInputChange }
+            />
+          </fieldset>
+
+          <fieldset className="att">
+            <legend> Atributos </legend>
+            <label htmlFor="att1">
+              Carisma:
+              <input
+                type="number"
+                data-testid="attr1-input"
+                id="att1"
+                value={ cardAttr1 }
+                name="cardAttr1"
+                onChange={ onInputChange }
+                placeholder="+1"
+              />
+            </label>
+
+            <label htmlFor="att2">
+              Força:
+              <input
+                type="number"
+                data-testid="attr2-input"
+                id="att2"
+                value={ cardAttr2 }
+                name="cardAttr2"
+                onChange={ onInputChange }
+                placeholder="+2"
+              />
+            </label>
+
+            <label htmlFor="att3">
+              Destreza:
+              <input
+                type="number"
+                data-testid="attr3-input"
+                id="att3"
+                value={ cardAttr3 }
+                name="cardAttr3"
+                onChange={ onInputChange }
+                placeholder="0"
+              />
+            </label>
+          </fieldset>
+
+          <fieldset>
+            <legend>Aparencia:</legend>
+            <input
+              data-testid="image-input"
+              type="text"
+              value={ cardImage }
+              name="cardImage"
+              onChange={ onInputChange }
+              placeholder="url"
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>Raridade</legend>
+            <select
+              data-testid="rare-input"
+              id="select"
+              value={ cardRare }
+              name="cardRare"
+              onChange={ onInputChange }
+            >
+              <option selected> normal </option>
+              <option>raro</option>
+              <option>muito raro</option>
+            </select>
+          </fieldset>
+
+          <div className="checkbox">
+            <label htmlFor="check">
+              Triunfante?
+              <input
+                id="check"
+                type="checkbox"
+                data-testid="trunfo-input"
+                checked={ cardTrunfo }
+                name="cardTrunfo"
+                onChange={ onInputChange }
+              />
+            </label>
+          </div>
+
+          <button
+            data-testid="save-button"
+            type="submit"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
+          >
+            Salvar
+          </button>
+        </form>
+      </section>
+    );
+  }
+}
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.string.isRequired,
+  isSaveButtonDisabled: PropTypes.func.isRequired,
+};
+
+export default Form;
